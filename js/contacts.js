ceb442f3546a7e30119be1ed991019bf0553b2d2
Rick Cerf <rickcerf@gmail.com>
2014-05-11 21:40:12 -0700

    deleting, and importing

--- js/contacts.js
+++ js/contacts.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+  var mainRegion = document.getElementById("main-region");
   var listBody = document.getElementsByTagName("tbody")[0];
   var contactTemplate = document.getElementById("contact-list-item");
+  var textArea = document.getElementsByClassName("textarea")[0];
   var contacts = [
       { id: 1, firstName: "Alice", lastName: "Arten",
         phoneNumber: "(415) 555-0184" },
@@ -9,24 +11,40 @@ document.addEventListener('DOMContentLoaded', function(){
       { id:3, firstName: "Charlie", lastName: "Campbell",
         phoneNumber: "(415) 555-0129" }
     ];
+  var events = {
+    "click .js-delete": function deleteClicked(e){
+      var item  = findItemIndex(this, contacts);
+      contacts = contacts.slice(0, item).concat(contacts.slice(item+1));
+      listBody.innerHTML = "";
+      addContactsToDom(contacts, contactTemplate, listBody, events);
+    },
+    "click .js-import": function importClicked(e){
+      var importArray = textArea.value.split(" ");
+      for(var i=0; i<importArray.length; i+=4){
+        var item = {};
+        item.firstName = importArray[i];
+        item.lastName = importArray[i+1];
+        item.phoneNumber = importArray[i+2] + " " + importArray[i+3];
+        contacts.push(item);
+      }
+      listBody.innerHTML = "";
+      addContactsToDom(contacts, contactTemplate, listBody, events);
+    },
+    "click .js-export": function exportClicked(e){
+      var collection = this;
+      var exportString = ""
+      for(var i=0; i<collection.length; i++){
+        var item = collection[i];
+        var entry = item.firstName + " " + item.lastName + " " + item.phoneNumber;
+        exportString += entry;
+      };
+      textArea.textContent = exportString;
+    },
+    "click .js-clear": function clearClicked(e){
+      textArea.textContent = "";
+    }
+  };
 
-  var interpolateTemplate = function(model, template){
-    var newNode = template.cloneNode(true);
-    var content = newNode.text.trim();
-    var interpolatingRegEx = /<%=([\s\S]+?)%>/g;
-
-    var interpolatedString = content.replace(interpolatingRegEx, function(match, variable, offset){
-      variable = variable.trim();
-      var interp= model[variable];
-      return interp;
-    });
-    return interpolatedString;
-  }
-
-  for(var i=0; i<contacts.length; i++){
-    var contact = contacts[i];
-    var interpolatedString = interpolateTemplate(contact, contactTemplate);
-    listBody.innerHTML = listBody.innerHTML + " " + interpolatedString;
-  }
-
+  bindListeners(mainRegion, events, contacts, true);
+  addContactsToDom(contacts, contactTemplate, listBody, events);
 });
---
+++ js/helpers.js
@@ -0,0 +1,55 @@
+var interpolateTemplate = function(model, template){
+    var newNode = template.cloneNode(true);
+    var content = newNode.text.trim();
+    var interpolatingRegEx = /<%=([\s\S]+?)%>/g;
+
+    var interpolatedString = content.replace(interpolatingRegEx, function(match, variable, offset){
+      variable = variable.trim();
+      var interp= model[variable];
+      return interp;
+    });
+    return interpolatedString;
+  };
+
+  var findItemIndex = function(model, collection){
+    for(var i=0; i<collection.length; i++){
+      var item = collection[i];
+      if(item.id === model.id){
+        return i;
+      }
+      console.log("model not found in collection");
+    }
+  }
+
+  var bindListeners = function(DOMTree, events, model, singleNode){
+    var latest_child = DOMTree.lastChild;
+    //iterate thru events
+    for(var key in events){
+      var typeAndSelector = key.split(" ");
+      var type = typeAndSelector[0];
+      var callback = events[key].bind(model);
+      var selector = typeAndSelector[1];
+      var targetNodes;
+      if(singleNode){
+        targetNodes = DOMTree.querySelectorAll(selector);
+      }else{
+       //find children elements of firstChild
+       targetNodes = latest_child.querySelectorAll(selector);
+      }
+      // iterate thru childNodes & attach eventListeners
+      for(var i=0; i<targetNodes.length; i++){
+        var targetNode = targetNodes[i];
+        targetNode.addEventListener(type, callback)
+      }
+    }
+  };
+
+  var addContactsToDom = function(contacts, contactTemplate, region, events){
+    for(var i=0; i<contacts.length; i++){
+      var contact = contacts[i];
+      var interpolatedString = interpolateTemplate(contact, contactTemplate);
+      region.insertAdjacentHTML("beforeend", interpolatedString);
+      bindListeners(region, events, contact);
+    }
+  };
+

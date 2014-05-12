1643bddaab928c8e3c02bdd2f99a7677d96d878f
Rick Cerf <rickcerf@gmail.com>
2014-05-12 14:40:20 -0700

    working edit button

--- js/contacts.js
+++ js/contacts.js
@@ -9,45 +9,37 @@ document.addEventListener('DOMContentLoaded', function(){
     ];
   var Contact = {
     id: window.localStorage.getItem("Contacts:index"),
+    currentContact: null,
 
     $region: document.getElementById("main-region"),
     $list: document.getElementsByTagName("tbody")[0],
-    $form: document.getElementsByClassName("textarea")[0],
+    $form: document.getElementsByTagName("textarea")[0],
     $contactTemplate: document.getElementById("contact-list-item"),
     $button_import: document.getElementsByClassName("js-import"),
     $button_discard: document.getElementsByClassName("js-discard"),
 
     events:  {
       "click .js-delete": function deleteClicked(e){
-        var item  = helpers.findItemIndex(this, contacts);
-        contacts = contacts.slice(0, item).concat(contacts.slice(item+1));
-        Contact.$list.innerHTML = "";
-        helpers.addContactsToDom(contacts, Contact.$contactTemplate, Contact.$list, Contact.events);
+        Contact.listDelete(this);
+      },
+      "click .js-edit": function(e){
+        Contact.listEdit(this);
       },
       "click .js-import": function importClicked(e){
-        var importArray = textArea.value.split(" ");
-        for(var i=0; i<importArray.length; i+=4){
-          var item = {};
-          item.firstName = importArray[i];
-          item.lastName = importArray[i+1];
-          item.phoneNumber = importArray[i+2] + " " + importArray[i+3];
-          contacts.push(item);
-        }
-        listBody.innerHTML = "";
-        helpers.addContactsToDom(contacts, Contact.$contactTemplate, Contact.$list, Contact.events);
+        Contact.listAdd();
       },
       "click .js-export": function exportClicked(e){
-        var collection = this;
-        var exportString = ""
-        for(var i=0; i<collection.length; i++){
-          var item = collection[i];
-          var entry = item.firstName + " " + item.lastName + " " + item.phoneNumber + ",\n";
-          exportString += entry;
-        };
-        textArea.value = exportString;
+        Contact.listExport(this);
       },
       "click .js-clear": function clearClicked(e){
-        textArea.textContent = "";
+        Contact.formClear();
+      },
+      "click .js-discard": function discarded(e){
+        if(!Contact.currentContact){
+          Contact.formClear();
+        }
+        Contact.listDelete(Contact.currentContact);
+        Contact.currentContact = null;
       }
     },
 
@@ -65,9 +57,42 @@ document.addEventListener('DOMContentLoaded', function(){
     storeEdit: function(entry){},
     storeRemove: function(entry){},
 
-    listAdd: function(entry){},
-    listEdit: function(entry){},
-    listDelete: function(entry){}
+    listAdd: function(entry){
+      var importArray = Contact.$form.value.split(" ");
+        for(var i=0; i<importArray.length; i+=4){
+          var item = {};
+          item.firstName = importArray[i];
+          item.lastName = importArray[i+1];
+          item.phoneNumber = importArray[i+2] + " " + importArray[i+3];
+          contacts.push(item);
+        }
+        Contact.$list.innerHTML = "";
+        helpers.addContactsToDom(contacts, Contact.$contactTemplate, Contact.$list, Contact.events);
+    },
+    listEdit: function(entry){
+      var exportString = entry.firstName + " " + entry.lastName + " " + entry.phoneNumber;
+      Contact.$form.value = "";
+      Contact.$form.value = exportString;
+      Contact.listDelete(entry);
+    },
+    listDelete: function(entry){
+      var item  = helpers.findItemIndex(entry, contacts);
+      contacts = contacts.slice(0, item).concat(contacts.slice(item+1));
+      Contact.$list.innerHTML = "";
+      helpers.addContactsToDom(contacts, Contact.$contactTemplate, Contact.$list, Contact.events);
+    },
+    listExport: function(collection){
+      var exportString = "";
+      for(var i=0; i<collection.length; i++){
+        var item = collection[i];
+        var entry = item.firstName + " " + item.lastName + " " + item.phoneNumber + ",\n";
+        exportString += entry;
+      };
+      textArea.value = exportString;
+    },
+    formClear: function(){
+      Contact.$form.value = "";
+    }
   };
 
   Contact.init();
